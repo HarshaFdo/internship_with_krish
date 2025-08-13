206e5d9f0fc4316c5f1dc1fa642bceaaa217aa99
HarshaFdo <fernando.harsha2016@gmail.com>
2025-08-13 23:16:33 +0530

    Alphabet Game

---
+++ Hot_Warm_Cool_Ice/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                "U", "V", "W", "X", "Y", "Z"};
+        Random random = new Random();
+        int index = random.nextInt(alphabet.length);
+        String randomLetter = alphabet[index];
+
+        int count = 0;
+
+        while(true) {
+            Scanner letter = new Scanner(System.in);
+            System.out.println("Guess the letter:");
+            String predictLetter = letter.nextLine().toUpperCase();
+
+            int predictNumber = Arrays.asList(alphabet).indexOf(predictLetter);
+
+            if (index == predictNumber) {
+                System.out.println("You are Right!");
+                break;
+            } else if (abs(index - predictNumber) <= 3) {
+                System.out.println("Hot");
+            } else if (abs(index - predictNumber) > 3 && abs(index - predictNumber) <= 6) {
+                System.out.println("Warm");
+            } else if (abs(index - predictNumber) > 6 && abs(index - predictNumber) <= 9) {
+                System.out.println("Cool");
+            } else {
+                System.out.println("Ice");
+            }
+            count++;
+        }
+        System.out.println("You tried " +count +"times to get the right answer");
+    }
+}
